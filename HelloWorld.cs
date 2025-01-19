46820d17b7d106d46c2409b82ac8c8b0ff8c36fb
GalaxyWolf951 <126086599+GalaxyWolf951@users.noreply.github.com>
2025-01-19 08:23:52 -0700

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Unity");
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+    }
+    
+    
+}
